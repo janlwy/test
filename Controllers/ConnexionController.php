f9d2a024b60413c505bcae17b9244fe3f0dfd182
Jean-Louis Marsillon <92369068+janlwy@users.noreply.github.com>
2024-10-22 16:01:02 +0200

    Add files via upload

---
+++ Controllers/ConnexionController.php
@@ -0,0 +1,57 @@
+<?php
+
+class ConnexionController
+{
+	public function connect()
+	{
+		// Informations de connexion à la base de données
+		$host = "localhost";
+		$dbname = "cda_projet";
+		$username = "root";
+		$password = "";
+
+		// On vérifie que le visiteur a correctement saisi puis envoyé le formulaire
+		if (isset($_POST['connexion']) && $_POST['connexion'] == 'Connexion') {
+			if ((isset($_POST['pseudo']) && !empty($_POST['pseudo'])) && (isset($_POST['mdp']) && !empty($_POST['mdp']))) {
+				// On se connecte à la bdd avec PDO
+				try {
+					$connexion = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+					$connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+					//Requête préparée pour éviter les injections SQL
+					$sql = 'SELECT * FROM users WHERE id=:pseudo';
+					$req = $connexion->prepare($sql);
+					$req->bindValue(':pseudo', $_POST['pseudo'], PDO::PARAM_STR);
+					$req->execute();
+					$user = $req->fetch(PDO::FETCH_ASSOC);
+
+					// Vérification du mot de passe avec password_verify()
+					if ($user && password_verify($_POST['mdp'], $user['mdp'])) {
+						session_start();
+						$_SESSION['pseudo'] = $_POST['pseudo'];
+						header('Location: ./Views/main/mediabox.php');
+						exit();
+					} else {
+						$erreur = 'Login ou mot de passe non reconnu !';
+						// Enregistrer l'erreur dans un fichier de log
+						logError("Erreur de connexion : " . $erreur);
+
+						// Rediriger l'utilisateur vers le formulaire de connexion avec un message d'erreur
+						$_SESSION['erreur'] = $erreur;
+						header('Location: connectForm.php'); 
+						exit();
+					}
+				} catch (PDOException $e) {
+					// Enregistrer l'erreur dans un fichier de log
+					logError("Erreur PDO : " . $e->getMessage());
+
+					// Afficher un message d'erreur générique à l'utilisateur
+					$_SESSION['erreur'] = "Une erreur est survenue lors de la connexion.";
+					header('Location: connectForm.php'); // Rediriger vers le formulaire de connexion
+					exit();
+				}
+			}
+		}
+	}
+}
+?>
