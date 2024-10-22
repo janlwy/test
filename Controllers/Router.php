f645bb7ba3f3904836b564fdfc3cf992103c1d73
janlwi <jlbin@outlook.fr>
2024-10-22 17:04:58 +0200

    feat: Enhance router to handle connection requests and default actions

--- Controllers/Router.php
+++ Controllers/Router.php
@@ -1,23 +1,29 @@
 <?php
-	class Router{
-		
-		public function __construct(){
-			//----capture de l'url en $_GET
-			// ?url=......
-			if(isset($_GET['url'])){
-				$url=$_GET['url'];
-				$urls=explode('/',$url);
-				$class=$urls[0];
-				
-			}else{
-				$url='';
-				$class='accueil';
-			}
-			$class=ucfirst($class);
-			$class=$class."Controller";
-			$class=new $class($url);
-			// echo "<h1 style='red'>Vous etes en instanciation</h1>";die;
+class Router
+{
+
+	public function __construct()
+	{
+		if (isset($_GET['url'])) {
+			$url = $_GET['url'];
+			$urls = explode('/', $url);
+			$class = $urls[0];
+		} else if (isset($_SERVER['REQUEST_URI']) && strpos($_SERVER['REQUEST_URI'], '/connexion') !== false) {
+			$class = 'Connexion@connect';
+		} else {
+			$url = '';
+			$class = 'Accueil';
 		}
-		
+
+		$controllerAction = explode('@', $class);
+		$controllerName = $controllerAction[0] . "Controller";
+		$actionName = isset($controllerAction[1]) ? $controllerAction[1] : 'index'; // méthode par défaut si non spécifiée
+
+		require_once "Controllers/$controllerName.php";
+		$controller = new $controllerName(); // Instancier sans argument
+		$controller->$actionName(); // Appeler la méthode connect()
+
 	}
+}
+
 ?>
