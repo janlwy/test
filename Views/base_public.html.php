327098bc766af98f9b9df1d55022f2d82229da5a
janlwi <jlbin@outlook.fr>
2024-10-22 17:09:15 +0200

    feat: Add base public HTML template and session management in index.php

---
+++ Views/base_public.html.php
@@ -0,0 +1,65 @@
+<?php
+// Démarrer la session au début du fichier
+session_start();
+/*
+if (isset($file) && isset($datas)) {
+	$content = generateFile($file, $datas);
+}*/
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="author" content="Jean-Louis Marsillon">
+	<meta name="description" content="Mon projet CDA">
+	<title>MédiaBox</title>
+	<link rel="icon" type="image/x-icon" href="/Ressources/favicon/favicon.ico">
+</head>
+<link rel="stylesheet" href="/css/media.css" type="text/css">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Display:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+<body class="clair-theme" id="themeBody">
+	<header>
+		<h1>Média Box</h1>
+	</header>
+	<main>
+		<nav class="menunav" id="myMenunav">
+			<a href="/" class="active">
+				<i class="material-icons">home</i>
+			</a>
+			<a href="?url=connexion" class="active">se connecter</a>
+			<a href="#">Musique</a>
+			<a href="#">Vidéos</a>
+			<a href="#">Photos</a>
+			<a href="#">Paroles</a>
+			<button type="button" id="boutonTheme" class="boutonTheme material-icons" title="no"></button>
+			<a href="deconnexion.php" class="sortie boutonright">
+				<i class="material-icons">logout</i>
+			</a>
+			<a href="javascript:void(0);" class="menuicon" onclick="myFunction()">
+				<i class="material-icons">menu</i>
+			</a>
+		</nav>
+		<section>
+			<?php if (isset($content)) echo $content; ?>
+		</section>
+	</main>
+	<footer>
+		<div class="socle">
+			<div class="icon-bar">
+				<a href="/mediabox.html" class="navlabel"><i class="material-icons">home</i>
+					<span>Accueil</span> </a>
+				<a href="/compte.html" class="navlabel"><i class="material-icons">face_6</i>
+					<span>Profile</span> </a>
+				<a href="#" class="navlabel"><i class="iconeTheme material-icons">brightness_4</i>
+					<span>Thème</span> </a>
+			</div>
+		</div>
+	</footer>
+	<script src="/js/script.js"></script>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
+session_start(); // Démarrer la session au début du point d'entrée
 
-	require_once "Fonctions/launch.php";
-	spl_autoload_register('charger');
-	
-	$router=new Router();
-	
+require_once "Fonctions/launch.php";
+spl_autoload_register('charger');
+
+$router = new Router();
+
+
 ?>
