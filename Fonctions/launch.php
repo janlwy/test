8f40a6fe8b5e55ac927c94800775b05225139cbd
janlwi <jlbin@outlook.fr>
2024-10-22 17:15:25 +0200

    refactor: Improve function structure and add error logging functionality

--- Fonctions/launch.php
+++ Fonctions/launch.php
@@ -1,41 +1,51 @@
 <?php
 
-	function charger($class){
-		$fileModels="Models/$class.php";
-		$fileControllers="Controllers/$class.php";
-		$fileViews="Views/$class.php";
-		$fileRessources="Ressources/$class.php";
-		$fileFonctions = "Fonctions/$class.php";
-		$files=array($fileModels,$fileControllers,$fileViews,$fileRessources, $fileFonctions);
-		foreach($files as $file){
-			if(file_exists($file)){
-				require_once($file);			
-			}
-
+function charger($class)
+{
+	$fileModels = "Models/$class.php";
+	$fileControllers = "Controllers/$class.php";
+	$fileViews = "Views/$class.php";
+	$fileRessources = "Ressources/$class.php";
+	$fileFonctions = "Fonctions/$class.php";
+	$files = array($fileModels, $fileControllers, $fileViews, $fileRessources, $fileFonctions);
+	foreach ($files as $file) {
+		if (file_exists($file)) {
+			include_once $file; // Utilisation de include_once
 		}
 	}
+}
 
-	function printr($tableaux){
-		echo "<h1><pre>";
-		print_r($tableaux);
-		echo "</pre>-----------------</h1>";//die;
-	}
-	
-	function generate($file,$datas,$layout="Views/base.html.php"){
-		$content=generateFile($file,$datas);
-		echo generateFile($layout,array('content'=>$content));
-	}
+function printr($tableaux)
+{
+	echo "<h1><pre>";
+	print_r($tableaux);
+	echo "</pre>-----------------</h1>";
+	//die;
+}
 
-	function generateFile($file, $datas) {
-		if(file_exists($file)){
-			ob_start();
-			extract($datas);
-			require_once($file);
-			return ob_get_clean();
-		}else{
-			
-			echo "Le fichier $file n'existe pas!";die;
-		}
-	}
+function generate($file, $datas, $layout = "Views/base_public.html.php")
+{
+	$content = generateFile($file, $datas);
+	return generateFile($layout, array('content' => $content)); // Retourner le contenu généré
+}
 
+function generateFile($file, $datas)
+{
+	if (file_exists($file)) {
+		ob_start();
+		extract($datas);
+		require_once $file;
+		return ob_get_clean();
+	} else {
+		echo "Le fichier $file n'existe pas!";
+		die;
+	}
+}
+
+function logError($message, $level = 'error')
+{
+	$logFile = 'errors.log'; // Nom du fichier de log
+	$logMessage = date('Y-m-d H:i:s') . " - [$level] - $message\n";
+	file_put_contents($logFile, $logMessage, FILE_APPEND);
+}
 ?>
