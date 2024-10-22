a1c6cad458deaf12b8cca02635127aab49a2aacb
janlwi <jlbin@outlook.fr>
2024-10-23 00:24:16 +0200

    fix: Update form action URL and adjust indentation in connectForm.php

--- Views/connect/connectForm.php
+++ Views/connect/connectForm.php
@@ -1,30 +1,30 @@
 <div>
 
-<form class="modalContenu Anime formFont" method="POST" action="/?url=connexion/connect">
+	<form class="modalContenu Anime formFont" method="POST" action="?url=mediabox/index">
 
-	<div class="formContainer">
-		<h2>Connexion à votre compte</h2>
-		<h3>Veuillez remplir ce formulaire pour vous connecter à votre compte.</h3>
-		<hr>
+		<div class="formContainer">
+			<h2>Connexion à votre compte</h2>
+			<h3>Veuillez remplir ce formulaire pour vous connecter à votre compte.</h3>
+			<hr>
 
-		<label for="pseudo"><b>Nom d'utilisateur</b></label>
-		<input class="inputModal" type="text" placeholder="Entrer un pseudonyme" name="pseudo" id="pseudo" required>
+			<label for="pseudo"><b>Nom d'utilisateur</b></label>
+			<input class="inputModal" type="text" placeholder="Entrer un pseudonyme" name="pseudo" id="pseudo" required>
 
-		<label for="mdp"><b>Mot de passe</b></label>
-		<input class="inputModal" type="password" placeholder="Entrer votre mot de passe" name="mdp" id="mdp" required>
+			<label for="mdp"><b>Mot de passe</b></label>
+			<input class="inputModal" type="password" placeholder="Entrer votre mot de passe" name="mdp" id="mdp" required>
 
-		<!-- <label for="repMdp"><b>Répéter le mot de passe</b></label>
+			<!-- <label for="repMdp"><b>Répéter le mot de passe</b></label>
 				<input class="inputModal" type="password" placeholder="Repeter le mot de passe" name="repeat" id="repMdp" required> -->
-		<hr>
+			<hr>
 
-		<p>En créant un compte vous acceptez de vous soumettre à nos <a href="#">conditions</a>.</p>
-		<button type="submit" class="modalButton" name="connexion" value="Connexion">Connexion</button>
-	</div>
+			<p>En créant un compte vous acceptez de vous soumettre à nos <a href="#">conditions</a>.</p>
+			<button type="submit" class="modalButton" name="connexion" value="Connexion">Connexion</button>
+		</div>
 
-	<div class="formContainer signin">
-		<p>Vous n'avez pas de compte ? <a href="/?url=connect/createUserForm">Création de compte</a>.</p>
-	</div>
+		<div class="formContainer signin">
+			<p>Vous n'avez pas de compte ? <a href="?url=connect/createUserForm">Création de compte</a>.</p>
+		</div>
 
-</form>
+	</form>
 
 </div>
