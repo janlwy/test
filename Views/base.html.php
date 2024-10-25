0a8fdcd49c3b361632ba8c93b5ca81ac17164fb8
janlwi <jlbin@outlook.fr>
2024-10-25 23:28:50 +0200

    25102024_2328

--- Views/base.html.php
+++ Views/base.html.php
@@ -71,7 +71,6 @@ if (session_status() === PHP_SESSION_NONE) {
 	</footer>
 
 	<script src="js/media.js"></script>
-	<script src="js/appCRUD.js"></script>
 	<script src="js/mediavideo.js"></script>
 	<script src="js/mediamusic.js"></script>
 	<noscript>Vous devez activer javascript pour une expérience complête de ce site</noscript>
