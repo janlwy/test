1557048cd8fa5ba85a4a8bc39c0b93f161ca3968
janlwi <jlbin@outlook.fr>
2024-10-25 17:06:19 +0200

    25102024_1706

--- Views/main/audioList.php
+++ Views/main/audioList.php
@@ -1,9 +1,9 @@
 <section>
     <span class="br"></span>
-    <a href='index.php?url=audio/insert' class='btnBase gris'>Ajouter</a>
+    <a href='index.php?url=audio/insert' class="btnBase gris">Ajouter</a>
     <?php echo $audioList; ?>
     <script>
         var audios = <?php echo json_encode($audios); ?>;
     </script>
-    <button id="play-selected">Lire la sélection</button>
+    <button id="play-selected" class="btnBase orange">Lire la sélection</button>
 </section>
